352f0577ec9c2957da8b9b962ce44c4c7e59069b
tawfiqsiam <toto.siam999@gmail.com>
2021-02-23 18:08:33 +0200

    Update dialogflow-setup.js

--- dialogflow-setup.js
+++ dialogflow-setup.js
@@ -4,7 +4,7 @@ const dialogflow = require('dialogflow')
 
 // Configuration
 const projectId = 'bumpit-dgvo'
-const sessionId = '2cb5c7a2ab447365be3a6cad355f0f5c236e178d'
+const sessionId = '110684607166208159124'
 
 async function detectAudioIntent(
   inputAudio,
